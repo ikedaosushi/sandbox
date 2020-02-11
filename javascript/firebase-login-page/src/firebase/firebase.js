fab51c32c7defbe08192c153fc55e8bc3a7ce0af
Ikeda Yutao <yutaro.ikeda@kaizenplatform.com>
2020-02-11 23:02:41 +0900

    Add

--- javascript/firebase-login-page/src/firebase/firebase.js
+++ javascript/firebase-login-page/src/firebase/firebase.js
@@ -3,7 +3,13 @@ import "firebase/auth";
 import "firebase/firestore";
 
 const firebaseConfig = {
-  //Your config values
+    apiKey: "AIzaSyBSiByly5HPoGuxNFdE-FVP3aUD9Y0ak7w",
+    authDomain: "tech-news-77085.firebaseapp.com",
+    databaseURL: "https://tech-news-77085.firebaseio.com",
+    projectId: "tech-news-77085",
+    storageBucket: "tech-news-77085.appspot.com",
+    messagingSenderId: "237188242486",
+    appId: "1:237188242486:web:3bba1d791850bac3c34c97"
 };
 
 export const myFirebase = firebase.initializeApp(firebaseConfig);
